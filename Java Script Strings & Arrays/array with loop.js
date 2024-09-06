af77a305091b4e80fd112c6ca15ffcb39004fe69
Kakashi <kakashisensay25@gmail.com>
2024-09-06 22:12:55 +0530

    Java Script Strings & Arrays

---
+++ Java Script Strings & Arrays/array with loop.js
@@ -0,0 +1,30 @@
+// array using loop
+
+let a1 = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a1[index];
+//     console.log(element)
+    
+// }
+
+// a1.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key]; 
+//         console.log(key, element)
+//     }
+// }
+
+for (const value of a) {
+    console.log(value)
+}
