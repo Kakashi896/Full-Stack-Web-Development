12ab5898b7a9d7f5ecbee669aad36e1a89abf6e0
Kakashi <kakashisensay25@gmail.com>
2024-09-06 22:12:23 +0530

    Java Script Intro

---
+++ Java Script Intro/New.js
@@ -0,0 +1,4 @@
+console.log("Hello world...");
+console.log("The code is running...");
+console.log("Typed by console.log...");
+console.log("And at the end jai shree ram...");
---
+++ Java Script Intro/script.js
@@ -0,0 +1,75 @@
+// alert("Jai shree Ram"); 
+
+// yashivlogs life
+// console.log("The code is running...");
+// console.log("Typed by console.log...");
+// console.log("And at the end jai shree ram...");
+
+// var a = prompt("enter your no...")
+// console.log("your no. is "  + a )
+
+// var isTrue= confirm("Are you sure you want to leave this site")
+ 
+// if(isTrue){
+//     console.log("your pc will now blast within 12 sec...🤯")
+// }
+// else{
+//     console.log("Yourr pc is safe now...😊")
+// };
+
+
+// document.title = "This is javascript..";
+// document.body.style.backgroundColor="yellow"
+
+
+console.log("sitaram")
+let a = 5;
+let b = 6;
+let c = "Akshat";
+// var 55a = "shivam" {Not Allowed}
+console.log(a + b);
+console.log(typeof a ,typeof b,typeof c);
+
+// const a1 = 6;
+// a1 = a1 + 1;   {Not Allowed}
+
+{
+    let a = 69;
+    console.log (a)  //print recent value of a because it works in loops (and it have block scope)
+}
+
+console.log(a); // print previous value of a
+
+// var z = 23; //var have globally scope
+
+// all data types;
+let x = "Sharingan";
+let y = 13;
+let z = 1.2;
+const p = 12;
+let q = true;
+let r = undefined;
+let s = null;
+
+console.log(x,y,z,p,q,r,s);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r,typeof s);
+
+// making an object
+
+let o = {
+    "name" : "Shivam Bhardwaj",
+    "employee id" : 705285,
+    "Is Handsome" : true,
+}
+console.log(o);
+
+// to add something in object
+
+o.salary ="At Internship";
+console.log(o);
+
+o.salary ="78 LPA";
+console.log(o);
+
+o.salary ="1.2 CPA";
+console.log(o);
