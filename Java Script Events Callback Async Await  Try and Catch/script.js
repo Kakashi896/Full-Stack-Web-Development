6de55772a8e9f72ababc000f9bd7294fc2d462ab
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-16 23:33:17 +0530

    Java Script Events Callback Async Await Try Catch

---
+++ Java Script Events Callback Async Await  Try and Catch/script.js
@@ -0,0 +1,129 @@
+let button = document.getElementById("btn")
+// List of all mouse events 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+
+button.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+})
+
+button.addEventListener("contextmenu", ()=>{
+    alert("Dont hack us by Right click Please")
+})
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e, e.key, e.keyCode)
+})
+
+
+
+
+
+
+// --------------------Promises 
+console.log('This is Promises');
+
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done")
+            resolve("Harry")
+        }, 3000);
+    }
+})
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 2")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done 2")
+            resolve("Harry 2")
+        }, 1000);
+    }
+})
+
+ 
+let p3 = Promise.race([prom1, prom2])
+p3.then((a)=>{
+    console.log(a)
+}).catch(err=>{
+    console.log(err)
+})
+
+
+
+
+
+
+
+
+// Async Await
+
+// async function getData() {
+//     // Simulate getting data from a server
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(455)
+//         }, 3500);
+//     })
+// }
+
+// settle means resolve or reject
+// resolve means promise has settled successfully
+// reject means promise has not settled successfully
+
+async function getData() {
+    // Simulate getting data from a server
+    // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+    let data = await x.json() 
+    return data
+}
+
+async function main(){
+    console.log("Loading modules")
+
+    console.log("Do something else")
+
+    console.log("Load data")
+
+    let data = await getData()
+
+    console.log(data)
+
+    console.log("process data")
+
+    console.log("task 2")
+
+}
+
+main()
+
+ 
+
+// data.then((v) => { 
+//     console.log(data)
+
+//     console.log("process data")
+
+//     console.log("task 2")
+ 
+// })
